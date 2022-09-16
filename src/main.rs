1f7075b6dd99cbad56e0f200bce2a678ef19ba78
Daniel Hoffman <dnaielh@intenda.za.com>
2022-09-16 21:52:16 +0200

    work, broken

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,94 @@
-use std::fs::File;
-use std::io::Write;
-use zip::result::ZipResult;
-use zip::write::FileOptions;
-use zip::ZipWriter;
+#![deny(warnings)]
 
-// Do this for each file, start_file to initialize followed by write_all for adding byte values.
-fn add_file(zip: &mut ZipWriter<File>, name: &str, content: &[u8]) {
-    zip.start_file(name, FileOptions::default());
-    zip.write_all(content);
+use std::net::SocketAddr;
+
+use bytes::Bytes;
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::server::conn::Http;
+use hyper::service::service_fn;
+use hyper::{Method, Recv, Request, Response, StatusCode};
+use tokio::net::TcpListener;
+
+/// This is our service handler. It receives a Request, routes on its
+/// path, and returns a Future of a Response.
+async fn echo(req: Request<Recv>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        // Serve some instructions at /
+        (&Method::GET, "/") => Ok(Response::new(full(
+            "Try POSTing data to /echo such as: `curl localhost:3000/echo -XPOST -d 'hello world'`",
+        ))),
+
+        // Simply echo the body back to the client.
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body().boxed())),
+
+        // TODO: Fix this, broken in PR #2896
+        // Convert to uppercase before sending back to client using a stream.
+        // (&Method::POST, "/echo/uppercase") => {
+        // let chunk_stream = req.into_body().map_ok(|chunk| {
+        //     chunk
+        //         .iter()
+        //         .map(|byte| byte.to_ascii_uppercase())
+        //         .collect::<Vec<u8>>()
+        // });
+        // Ok(Response::new(Body::wrap_stream(chunk_stream)))
+        // }
+
+        // Reverse the entire body before sending back to the client.
+        //
+        // Since we don't know the end yet, we can't simply stream
+        // the chunks as they arrive as we did with the above uppercase endpoint.
+        // So here we do `.await` on the future, waiting on concatenating the full body,
+        // then afterwards the content can be reversed. Only then can we return a `Response`.
+        (&Method::POST, "/echo/reversed") => {
+            // To protect our server, reject requests with bodies larger than
+            // 64kbs of data.
+            let max = req.body().size_hint().upper().unwrap_or(u64::MAX);
+            if max > 1024 * 64 {
+                let mut resp = Response::new(full("Body too big"));
+                *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
+                return Ok(resp);
+            }
+
+            let whole_body = hyper::body::to_bytes(req.into_body()).await?;
+
+            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
+            Ok(Response::new(full(reversed_body)))
+        }
+
+        // Return the 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
-fn main() {
-    fn doit() {
-        // For this example we write to a buffer, but normally you should use a File
-        let mut buf: &mut [u8] = &mut [0u8; 65536];
-        println!("{:?}", std::env::current_dir());
-        let mut file = match File::create("./src/testFiles/zippedFile.zip") {
-            Err(e) => panic!("{}", e),
-            Ok(file) => file,
-        };
-        let mut zip = zip::ZipWriter::new(file);
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
 
-        add_file(
-            &mut zip,
-            "innerFile1.json",
-            b"{testKey: 'testValue', testSecondKey: true}",
-        );
-        add_file(
-            &mut zip,
-            "innerFile2.json",
-            b"{test2Key1: 'testValue', test2SecondKey: true}",
-        );
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
 
-        zip.finish();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = Http::new().serve_connection(stream, service_fn(echo)).await {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
     }
-
-    println!("Result: {:?}", doit());
 }
